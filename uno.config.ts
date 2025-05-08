5b3b3aa997fd90417b9df6637e25f60c00edca45
tunnckoCore <5038030+tunnckoCore@users.noreply.github.com>
2025-05-08 08:03:03 +0300

    fix: expose the unocss config, clean deps
    Signed-off-by: tunnckoCore <5038030+tunnckoCore@users.noreply.github.com>

--- uno.config.ts
+++ uno.config.ts
@@ -1,12 +1,11 @@
+/* eslint-disable node/no-unpublished-import */
 import presetWind4 from '@unocss/preset-wind4';
 import { defineConfig } from 'unocss';
 
 export default defineConfig({
   presets: [
     presetWind4({
-      preflights: {
-        reset: true,
-      }
+      preflights: { reset: true },
     }),
   ],
 });
